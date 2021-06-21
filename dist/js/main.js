27684abcd508dcae76f86499ba927151ffbf7206
Anna <anna.vl.gromova@gmail.com>
2021-06-21 14:22:08 +0300

    promotion timer set & modal appear

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,7 +21,7 @@ function animOnScroll() {
         const animItem = animItems[i];
         const animItemHeight = animItem.offsetHeight;
         const animItemOffset = offset(animItem).top;
-        const animStart = 8;
+        const animStart = 6;
         
         let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
@@ -39,5 +39,87 @@ function animOnScroll() {
     }
 }
 
+// Promotion timer
+const deadline = '2021-06-29';
+
+function getTimeRemaining(endtime) {
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor( (t/(1000*60*60*24)) ),
+        seconds = Math.floor( (t/1000) % 60 ),
+        minutes = Math.floor( (t/1000/60) % 60 ),
+        hours = Math.floor( (t/(1000*60*60) % 24) );
+
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function getZero(num){
+
+    return num = (num >= 0 && num < 10) ? `0${num}` : num;
+}
+
+function setClock(selector, endtime) {
+
+    const timer = document.querySelector(selector),
+        days = timer.querySelector("#days"),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        days.innerHTML = getZero(t.days);
+        hours.innerHTML = getZero(t.hours);
+        minutes.innerHTML = getZero(t.minutes);
+        seconds.innerHTML = getZero(t.seconds);
+
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('.timer', deadline);
 
 
+// MODAL
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', function() {
+        modal.classList.toggle('show');
+        
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+function closeModal() {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';
+}
+    
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === "Escape" && modal.classList.contains('show')) { 
+        closeModal();
+    }
+});
