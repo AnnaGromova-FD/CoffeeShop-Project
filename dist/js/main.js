53f2fbde8a7b92b42343804098618b9c5ac9435b
Anna <anna.vl.gromova@gmail.com>
2021-06-24 11:47:50 +0300

    responsive design add

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,4 @@
 
-
 //Animation on scroll
 const animItems = document.querySelectorAll('._anim-items');
 
@@ -123,3 +122,134 @@ document.addEventListener('keydown', (event) => {
         closeModal();
     }
 });
+
+
+// Tabs with filter by country origin
+let tabs = document.querySelectorAll('.country-tab'),
+    tabsContent = document.querySelectorAll('.coffee__item'),
+    tabsParent = document.querySelector('.country-wrap');
+
+function hideTabContent() {
+    tabsContent.forEach(item => {
+        // item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(item => {
+        item.classList.remove('active');
+    });
+}
+
+function showTabContent(i = 0) {
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('active');
+}
+    
+hideTabContent();
+showTabContent();
+
+tabsParent.addEventListener('click', function(event) {
+    const target = event.target;
+
+    // isFedor['Пользователь с именем Федор'] = users.filter(item => item.name.startsWith('Fedor'));
+    if(target && target.classList.contains('country-tab')) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+
+    // target.dataset.country === 'kenya')
+    if(target && target.classList.contains('country-tab')) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+
+    if(target && target.classList.contains('country-tab')) {
+        tabs.forEach((item, i) => {
+            if (target == item) {
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+
+// Calculator
+
+// // Calculator
+
+//     const result = document.querySelector('.calculating__result span');
+//     let sex = 'female',
+//         height, weight, age,
+//         ratio = 1.375;
+
+//     function calcTotal() {
+//         if (!sex || !height || !weight || !age || !ratio) {
+//             result.textContent = '____'; // Можете придумать что угодно
+//             return;
+//         }
+//         if (sex === 'female') {
+//             result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+//         } else {
+//             result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+//         }
+//     }
+
+//     calcTotal();
+
+//     function getStaticInformation(parentSelector, activeClass) {
+//         const elements = document.querySelectorAll(`${parentSelector} div`);
+
+//         elements.forEach(elem => {
+//             elem.addEventListener('click', (e) => {
+//                 if (e.target.getAttribute('data-ratio')) {
+//                     ratio = +e.target.getAttribute('data-ratio');
+//                 } else {
+//                     sex = e.target.getAttribute('id');
+//                 }
+    
+//                 elements.forEach(elem => {
+//                     elem.classList.remove(activeClass);
+//                 });
+    
+//                 e.target.classList.add(activeClass);
+    
+//                 calcTotal();
+//             });
+//         });
+//     }
+
+//     getStaticInformation('#gender', 'calculating__choose-item_active');
+//     getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+//     function getDynamicInformation(selector) {
+//         const input = document.querySelector(selector);
+
+//         input.addEventListener('input', () => {
+//             switch(input.getAttribute('id')) {
+//                 case "height":
+//                     height = +input.value;
+//                     break;
+//                 case "weight":
+//                     weight = +input.value;
+//                     break;
+//                 case "age":
+//                     age = +input.value;
+//                     break;
+//             }
+
+//             calcTotal();
+//         });
+//     }
+
+//     getDynamicInformation('#height');
+//     getDynamicInformation('#weight');
+//     getDynamicInformation('#age');
